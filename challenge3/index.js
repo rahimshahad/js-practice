fd511b2998e791c9b45482c82e47998d75288870
rahimshahad <rahim.shahad2@gmail.com>
2022-02-28 17:04:40 -0500

    recalculate winner with minimum score requirement

--- challenge3/index.js
+++ challenge3/index.js
@@ -24,3 +24,18 @@ if(avgKoalas > avgDolphins){
 } else {
     console.log("It's a tie!")
 }
+
+// Bonus: There's a minimum score requirement to determine winner
+
+const min = 100
+
+if(avgKoalas >= min && avgKoalas > avgDolphins){
+    console.log("Koalas win!")
+}else if(avgDolphins >= min && avgDolphins > avgKoalas){
+    console.log("Dolphins win!")
+}else if(avgKoalas >= min && avgDolphins >= min && avgDolphins === avgKoalas){
+    console.log("It's a draw!")
+    
+}else {
+    console.log("There is no winner :(")
+}
