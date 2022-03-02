c16fb82fa9dfd966768ba7c5f5c446f6ad5a71b2
rahimshahad <rahim.shahad2@gmail.com>
2022-03-02 07:01:13 -0500

    done

---
+++ challenge5/index.js
@@ -0,0 +1,26 @@
+const calcAverage = (scores) =>{
+    let sum = 0;
+    for (i = 0; i < scores.length; i++){
+        sum += scores[i]
+    }
+    return sum/scores.length
+}
+
+const dolphinsScores = [85, 54, 41]
+const koalasScores = [23, 34, 27]
+
+const dolphinsAvg = calcAverage(dolphinsScores)
+const koalasAvg = calcAverage(koalasScores)
+
+console.log(dolphinsAvg, koalasAvg)
+
+function checkWinner(dolphinsAvg, koalasAvg){
+    if(dolphinsAvg >= koalasAvg *2){
+        console.log(`Dolphins win (${dolphinsAvg} vs ${koalasAvg})`)
+    } else if (koalasAvg >= dolphinsAvg *2){
+        console.log(`Koalas win (${koalasAvg} vs ${dolphinsAvg})`)
+    } else{
+        console.log("There is no winner")
+    }
+}
+checkWinner(dolphinsAvg, koalasAvg)
