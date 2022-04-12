31daa2ff3a1ba109f004fc26c4250fca3d560b8e
rahimshahad <rahim.shahad2@gmail.com>
2022-04-12 02:37:17 -0400

    done

---
+++ challenge7/index.js
@@ -0,0 +1,27 @@
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi
+    }
+}
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+mark.calcBMI()
+console.log(mark.bmi)
+
+john.calcBMI()
+console.log(john.bmi)
+ 
+console.log(mark.bmi > john.bmi ? `${mark.fullName}'s BMI(${mark.bmi}) is higher than ${john.fullName}'s(${john.bmi})` : `${john.fullName}'s BMI(${john.bmi}) is higher than ${mark.fullName}'s(${mark.bmi})`)
