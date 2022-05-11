4857577b3ecd0eb6549ee9780ef4f4ef7922eec5
rahimshahad <rahim.shahad2@gmail.com>
2022-05-11 12:59:43 -0400

    calculate averge with function

--- challenge8/index.js
+++ challenge8/index.js
@@ -10,3 +10,14 @@ for(i = 0; i < bills.length; i++){
 }
 
 console.log(totals)
+
+function calcAverage (arr){
+    let sum = 0
+    for(i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    const avg = sum/arr.length
+    console.log(avg)
+}
+
+calcAverage(totals)
